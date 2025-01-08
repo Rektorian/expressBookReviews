9ad3d30497565f8376fadeb82b4cf1b4fad4ae48
Pawel Baczynski <pawel.baczynski86@gmail.com>
2025-01-08 15:26:23 +0100

    Uploading changes

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -5,24 +5,86 @@ const regd_users = express.Router();
 
 let users = [];
 
-const isValid = (username)=>{ //returns boolean
-//write code to check is the username is valid
+const isValid = (username) => { //returns boolean
+  //write code to check is the username is valid
 }
 
-const authenticatedUser = (username,password)=>{ //returns boolean
-//write code to check if username and password match the one we have in records.
+const authenticatedUser = (username, password) => { //returns boolean
+  //write code to check if username and password match the one we have in records.
+  // Filter the users array for any user with the same username and password
+  let validusers = users.filter((user) => {
+    return (user.username === username && user.password === password);
+  });
+  // Return true if any valid user is found, otherwise false
+  if (validusers.length > 0) {
+    return true;
+  } else {
+    return false;
+  }
 }
 
 //only registered users can login
-regd_users.post("/login", (req,res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+regd_users.post("/login", (req, res) => {
+  console.log("inside /login");
+  const username = req.body.username;
+  const password = req.body.password;
+
+  // Check if username or password is missing
+  if (!username || !password) {
+    return res.status(404).json({ message: "Error logging in" });
+  }
+
+  // Authenticate user
+  if (authenticatedUser(username, password)) {
+    // Generate JWT access token
+    let accessToken = jwt.sign({
+      data: password
+    }, 'access', { expiresIn: 60 * 60 });
+
+    // Store access token and username in session
+    req.session.authorization = {
+      accessToken, username
+    }
+    return res.status(200).send("User successfully logged in");
+  } else {
+    return res.status(208).json({ message: "Invalid Login. Check username and password" });
+  }
 });
 
 // Add a book review
 regd_users.put("/auth/review/:isbn", (req, res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  console.log("Inside /auth/review/:isbn");
+  const isbn = req.params.isbn;
+  const username = req.session.authorization.username;
+  let filtered_book = books[isbn]; // Retrieve book object associated with isbn
+
+  if(filtered_book) {
+    let review_to_be_added = req.query.review;
+    if (review_to_be_added) {
+      filtered_book.reviews[username] = review_to_be_added; // creates a new property in the reviews object with the username as the key and the review_to_be_added as the value
+      books[isbn] = filtered_book; // updates the books array with the filtered_book object
+    }
+    res.send(`Review for book with isbn: ${isbn} added/updated!`)
+  } else {
+    res.send(`Unable to find book for provided isbn: ${isbn}.`);
+  }
+});
+
+// Delete book review
+regd_users.delete("/auth/review/:isbn", (req, res) => {
+  console.log("Inside delete /auth/review/:isbn");
+  const isbn = req.params.isbn;
+  const username = req.session.authorization.username;
+  console.log(`username is ${username}`);
+  let filtered_book = books[isbn]; // Retrieve book object associated with isbn
+  console.log(books[isbn].reviews[username])
+
+  if(filtered_book) {
+    delete books[isbn].reviews[username]
+    res.send(`Review for book with isbn: ${isbn} deleted!`)
+  } else {
+    res.send(`Unable to find book for provided isbn: ${isbn}.`);
+  }
 });
 
 module.exports.authenticated = regd_users;
--- final_project/router/general.js
+++ final_project/router/general.js
@@ -1,43 +1,197 @@
+// Import the axios library
+const axios = require('axios');
 const express = require('express');
 let books = require("./booksdb.js");
 let isValid = require("./auth_users.js").isValid;
 let users = require("./auth_users.js").users;
 const public_users = express.Router();
 
+// Check if a user with the given username already exists
+const doesExist = (username) => {
+  // Filter the users array for any user with the same username
+  let userswithsamename = users.filter((user) => {
+    return user.username === username;
+  });
+  // Return true if any user with the same username is found, otherwise false
+  if (userswithsamename.length > 0) {
+    return true;
+  } else {
+    return false;
+  }
+}
 
-public_users.post("/register", (req,res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+public_users.post("/register", (req, res) => {
+  console.log("inside /register");
+  const username = req.body.username;
+  const password = req.body.password;
+
+  if (username && password) {
+    // Check if the user does not already exist
+    if (!doesExist(username)) {
+      // Add the new user to the users array
+      users.push({ "username": username, "password": password });
+      return res.status(200).json({ message: "User successfully registered. Now you can login" });
+    } else {
+      return res.status(404).json({ message: "User already exists!" });
+    }
+
+  } else {
+    // Return error if username or password is missing
+    return res.status(404).json({ message: "Unable to register user." });
+  }
 });
 
-// Get the book list available in the shop
-public_users.get('/',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+// =============================================================>GET books list<=============================================================================
+
+// // Get the book list available in the shop -> BASIC approach
+// public_users.get('/', function (req, res) {
+//   res.send(JSON.stringify(books, null, 4));
+// });
+
+// Get the book list available in the shop using async/await and Axios -> AXIOS approach 1
+public_users.get('/', async function (req, res) {
+  axios.get("http://localhost:5000/internal-books")
+  // If the request is successful, the `.then` block is executed.
+    .then(response => {
+      // The response object contains the data returned from the server.
+      res.send(JSON.stringify(response.data, null, 4));
+    })
+    // If there is an error during the request, the `.catch` block is executed.
+    .catch(error => {
+      // We log an error message to the console along with the error object.
+      // This helps in debugging and understanding what went wrong with the request.
+      console.error('Error fetching book list:', error);
+      res.status(500).send('Error fetching book list');
+    });
 });
 
-// Get book details based on ISBN
-public_users.get('/isbn/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
- });
-  
-// Get book details based on author
-public_users.get('/author/:author',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+// // Get the book list available in the shop using async/await and Axios -> AXIOS approach 2
+// public_users.get('/', async function (req, res) {
+//   try {
+//     const response = await axios.get("http://localhost:5000/internal-books");
+//     res.send(JSON.stringify(response.data, null, 4));
+//   } catch (error) {
+//     console.error('Error fetching book list:', error);
+//     res.status(500).send('Error fetching book list');
+//   }
+// });
+
+// Separate route to serve the books data for axios.get() to simulate external call -> AXIOS approach
+public_users.get('/internal-books', function (req, res) {
+  res.send(JSON.stringify(books, null, 4));
 });
 
-// Get all books based on title
-public_users.get('/title/:title',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+// =============================================================>GET books list by ISBN<=============================================================================
+
+// // Get book details based on ISBN -> BASIC approach
+// public_users.get('/isbn/:isbn', function (req, res) {
+//   const isbn = req.params.isbn;
+//   res.send(books[isbn])
+// });
+
+// Get book details based on ISBN using async/await and Axios -> AXIOS approach
+public_users.get('/isbn/:isbn', async function (req, res) {
+  try {
+    const isbn = req.params.isbn;
+    const response = await axios.get(`http://localhost:5000/internal-isbn/${isbn}`);
+    res.send(response.data);
+  } catch (error) {
+    console.error('Error fetching books list by ISBN', error);
+    res.status(500).send('Error fetching books list by ISBN');
+  }
 });
+
+// Separate route to serve the books data by ISBN for axios.get() to simulate external call -> AXIOS approach
+public_users.get('/internal-isbn/:isbn', function (req, res) {
+  const isbn = req.params.isbn;
+  res.send(books[isbn]);
+});
+
+// =============================================================>GET books list by Author<=============================================================================
+
+// // Get book details based on author -> BASIC approach
+// // When using Postman, you can manually encode the spaces in the URL by replacing them with %20. 
+// // For example, if you want to pass a title like "Super House", you should enter it in the URL as "Super%20House":
+// // http://localhost:5000/title/Super%20House
+// public_users.get('/author/:author', function (req, res) {
+//   const author_to_find = req.params.author.toLowerCase();
+//   console.log(`author_to_find is ${author_to_find}`);
+//   let filteredBooks = Object.values(books).filter(book => book.author.toLowerCase() === author_to_find);
+//   console.log(filteredBooks);
+
+//   res.send(filteredBooks);
+// });
+
+// Get book details based on author using async/await and Axios -> AXIOS approach
+public_users.get('/author/:author', async function (req, res) {
+  const author_to_find = req.params.author.toLowerCase();
+  axios.get(`http://localhost:5000/internal-author/${author_to_find}`)
+  // If the request is successful, the `.then` block is executed.
+    .then(response => {
+      // The response object contains the data returned from the server.
+      res.send(response.data);
+    })
+    // If there is an error during the request, the `.catch` block is executed.
+    .catch(error => {
+      // We log an error message to the console along with the error object.
+      // This helps in debugging and understanding what went wrong with the request.
+      console.error('Error fetching books list based on author:', error);
+      res.status(500).send('Error fetching books list based on author');
+    });
+});
+
+// Separate route to serve book details based on author for axios.get() to simulate external call -> AXIOS approach
+public_users.get('/internal-author/:author', function (req, res) {
+  const author_to_find = req.params.author;
+  let filteredBooks = Object.values(books).filter(book => book.author.toLowerCase() === author_to_find);
+  res.send(filteredBooks);
+});
+
+// =============================================================>GET books list by Title<=============================================================================
+
+// // Get all books based on title -> BASIC approach
+// public_users.get('/title/:title', function (req, res) {
+//   const title_to_find = req.params.title.toLowerCase();
+//   console.log(`title_to_find is ${title_to_find}`);
+//   let filteredBooks = Object.values(books).filter(book => book.title.toLowerCase() === title_to_find);
+
+//   res.send(filteredBooks)
+// });
+
+// Get book details based on title using async/await and Axios -> AXIOS approach
+public_users.get('/title/:title', async function (req, res) {
+  const title_to_find = req.params.title.toLowerCase();
+  axios.get(`http://localhost:5000/internal-title/${title_to_find}`)
+  // If the request is successful, the `.then` block is executed.
+    .then(response => {
+      // The response object contains the data returned from the server.
+      res.send(response.data);
+    })
+    // If there is an error during the request, the `.catch` block is executed.
+    .catch(error => {
+      // We log an error message to the console along with the error object.
+      // This helps in debugging and understanding what went wrong with the request.
+      console.error('Error fetching books list based on title:', error);
+      res.status(500).send('Error fetching books list based on title');
+    });
+});
+
+// Separate route to serve book details based on title for axios.get() to simulate external call -> AXIOS approach
+public_users.get('/internal-title/:title', function (req, res) {
+  const title_to_find = req.params.title;
+  let filteredBooks = Object.values(books).filter(book => book.title.toLowerCase() === title_to_find);
+  res.send(filteredBooks);
+});
+
+// =============================================================>GET books review by ISBN<=============================================================================
 
 //  Get book review
-public_users.get('/review/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+public_users.get('/review/:isbn', function (req, res) {
+  const isbn = req.params.isbn.toLowerCase();
+  console.log(`isbn_review_to_find is ${isbn}`);
+  const reviews = books[isbn]["reviews"];
+
+  res.send(reviews)
 });
 
 module.exports.general = public_users;
